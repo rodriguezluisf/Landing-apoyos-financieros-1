9287bb8ea2aa8b57985e3a41e036b28114b3e1cd
seguragabriel <seguragabriel@unbosque.edu.co>
2020-07-06 16:08:36 -0500

    Desarrollo Julio 6

---
+++ js/custom.js
@@ -0,0 +1,229 @@
+(function ($) {
+	
+	// -------------
+	// Accordion icetex
+	
+	$(".acordion1 a").click(function() {
+		$(".content_accordion_1").toggleClass("open");
+		$(".acordion1 a").toggleClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_1 .close-button").click(function() {
+		$(".content_accordion_1").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion1 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion pichincha
+	
+	$(".acordion2 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').toggleClass('open');
+		$('.acordion2 a').toggleClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_2 .close-button").click(function() {
+		$(".content_accordion_2").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion2 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion financiar
+	
+	$(".acordion3 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').toggleClass('open');
+		$('.acordion3 a').toggleClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_3 .close-button").click(function() {
+		$(".content_accordion_3").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion3 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion fincomercio
+	
+	$(".acordion4 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').toggleClass('open');
+		$('.acordion4 a').toggleClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_4 .close-button").click(function() {
+		$(".content_accordion_4").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion4 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion michelsen
+	
+	$(".acordion5 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').toggleClass('open');
+		$('.acordion5 a').toggleClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_5 .close-button").click(function() {
+		$(".content_accordion_5").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion5 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion banco de bogota
+	
+	$(".acordion6 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').toggleClass('open');
+		$('.acordion6 a').toggleClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_6 .close-button").click(function() {
+		$(".content_accordion_6").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion6 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion sufi
+	
+	$(".acordion7 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').toggleClass('open');
+		$('.acordion7 a').toggleClass('open');
+		$('.content_accordion_8').removeClass('open');
+		$('.acordion8 a').removeClass('open');
+	});
+	
+	$(".content_accordion_7 .close-button").click(function() {
+		$(".content_accordion_7").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion7 a").toggleClass("open");
+	});
+	
+	// -------------
+	// Accordion lumni
+	
+	$(".acordion8 a").click(function() {
+		$(".content_accordion_1").removeClass("open");
+		$(".acordion1 a").removeClass("open");
+		$('.content_accordion_2').removeClass('open');
+		$('.acordion2 a').removeClass('open');
+		$('.content_accordion_3').removeClass('open');
+		$('.acordion3 a').removeClass('open');
+		$('.content_accordion_4').removeClass('open');
+		$('.acordion4 a').removeClass('open');
+		$('.content_accordion_5').removeClass('open');
+		$('.acordion5 a').removeClass('open');
+		$('.content_accordion_6').removeClass('open');
+		$('.acordion6 a').removeClass('open');
+		$('.content_accordion_7').removeClass('open');
+		$('.acordion7 a').removeClass('open');
+		$('.content_accordion_8').toggleClass('open');
+		$('.acordion8 a').toggleClass('open');
+	});
+	
+	$(".content_accordion_8 .close-button").click(function() {
+		$(".content_accordion_8").toggleClass("open");
+		$(".content_accordion").toggleClass("open");
+		$(".acordion8 a").toggleClass("open");
+	});
+	
+	
+})(jQuery);
+
